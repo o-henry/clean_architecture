8d4d77a0b186bd1a33a0c8262fdfb0a68003107a
Henry <c.henry.9209@gmail.com>
2024-10-11 16:25:27 +0900

    Initial Commit

---
+++ CA/CAApp.swift
@@ -0,0 +1,17 @@
+//
+//  CAApp.swift
+//  CA
+//
+//  Created by Henry on 10/11/24.
+//
+
+import SwiftUI
+
+@main
+struct CAApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CATests/CATests.swift
@@ -0,0 +1,17 @@
+//
+//  CATests.swift
+//  CATests
+//
+//  Created by Henry on 10/11/24.
+//
+
+import Testing
+@testable import CA
+
+struct CATests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
